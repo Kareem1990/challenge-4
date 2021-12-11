0037a411a77bea455c90bb3c10b3d780810d0a88
Kareem Rizk <kareem.magdy5@gmail.com>
2021-12-11 17:00:05 -0500

    questions modifications

--- script.js
+++ script.js
@@ -9,27 +9,27 @@ var quesOle = quesEl.querySelector("#quesOl");
 // let timerEl = document.querySelector(".timer");
 
 
-quesOle.textContent = "Are you a human?"
+quesOle.textContent = "Arrays in Javascript can be used to store ?"
 
 
 var bt1 = document.createElement("button")
-bt1.textContent = "1- Yes";
+bt1.textContent = "1- All";
 quesOle.appendChild(bt1);
 bt1.classList.add('button-class')
 bt1.className="button-class";
 
 var bt2 = document.createElement("button");
-bt2.textContent = "2- no";
+bt2.textContent = "2- other arrays";
 quesOle.appendChild(bt2);
 bt2.className="button-class";
 
 var bt3 = document.createElement("button");
-bt3.textContent = "3- Perhaps";
+bt3.textContent = "3- booleans";
 quesOle.appendChild(bt3);
 bt3.className="button-class";
 
 var bt4 = document.createElement("button");
-bt4.textContent = "4- I Don't know!";
+bt4.textContent = "4- numbers and strings";
 quesOle.appendChild(bt4);
 bt4.className="button-class";
 
--- script3.js
+++ script3.js
@@ -6,27 +6,26 @@ var quesEl = document.querySelector(".page-content")
 
 var quesOle = quesEl.querySelector("#quesOl");
 
-quesOle.textContent = "Are you an Animal?"
-
+quesOle.textContent = "Commonly used data types don’t  include?"
 
 var bt1 = document.createElement("button")
-bt1.textContent = "1- Yes";
+bt1.textContent = "1- strings";
 quesOle.appendChild(bt1);
 bt1.classList.add('button-class')
 bt1.className="button-class";
 
 var bt2 = document.createElement("button");
-bt2.textContent = "2- no";
+bt2.textContent = "2- alert";
 quesOle.appendChild(bt2);
 bt2.className="button-class";
 
 var bt3 = document.createElement("button");
-bt3.textContent = "3- Perhaps";
+bt3.textContent = "3- booleans";
 quesOle.appendChild(bt3);
 bt3.className="button-class";
 
 var bt4 = document.createElement("button");
-bt4.textContent = "4- I Don't know!";
+bt4.textContent = "4- num";
 quesOle.appendChild(bt4);
 bt4.className="button-class";
 
@@ -54,6 +53,8 @@ function myTimer( )
 {alert("Time out, You lose");
 location.href = "index.html";
 }
+
+
 
 
 
